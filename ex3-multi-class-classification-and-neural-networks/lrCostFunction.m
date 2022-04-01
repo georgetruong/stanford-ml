451dba4473c31af6eee1a6f85ea818c6a66c2477
George Truong <truong.george@gmail.com>
2022-03-31 21:02:55 -0700

    Vectorized cost function, gradient function, and regularized logistic regression

--- ex3-multi-class-classification-and-neural-networks/lrCostFunction.m
+++ ex3-multi-class-classification-and-neural-networks/lrCostFunction.m
@@ -35,16 +35,17 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
+[m, n] = size(X);
+h = sigmoid(X * theta);
+j_reg = (lambda/(2*m)) * sum(theta(2:n).^2);
+cost = sum(-y.*log(h) - (1-y).*log(1-h)) / m;
+J = cost + j_reg;
 
-
-
-
-
-
-
-
-
-
+grad = sum((h-y) .* X) / m;
+temp = theta;
+temp(1) = 0;
+g_reg = ((lambda/m) .* temp');
+grad = grad + g_reg;
 % =============================================================
 
 grad = grad(:);
