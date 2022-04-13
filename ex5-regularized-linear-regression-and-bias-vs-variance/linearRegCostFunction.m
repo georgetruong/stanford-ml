3311472ced0fa51e10a791b6adf284c372eb6486
George Truong <truong.george@gmail.com>
2022-04-13 14:37:31 -0700

    Regularized linear regression cost function and gradient

--- ex5-regularized-linear-regression-and-bias-vs-variance/linearRegCostFunction.m
+++ ex5-regularized-linear-regression-and-bias-vs-variance/linearRegCostFunction.m
@@ -19,16 +19,21 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+% Set bias to 0 for regularization calculations
+theta_tmp = theta;
+theta_tmp(1) = 0;
 
+h = X * theta;
 
+% Calculate cost with regularization
+J = (1/(2*m)) * sum((h-y) .^ 2);
+J_reg = (lambda/(2*m)) * sum(theta_tmp .^ 2);
+J = J + J_reg;
 
-
-
-
-
-
-
-
+% Calculate gradient with regulation
+grad_reg = (lambda/m) * theta_tmp';
+grad = ((1/m) * sum((h-y) .* X));
+grad = grad + grad_reg;
 
 % =========================================================================
 
